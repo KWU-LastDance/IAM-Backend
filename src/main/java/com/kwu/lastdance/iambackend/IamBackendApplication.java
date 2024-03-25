a2c346d4cce86c2204d8d696b06ed6675606a2d7
Hyunjun KIM <hihj070914@icloud.com>
2024-03-25 23:22:36 +0900

    feat: initialize project
    - Spring Initializr 를 통해 보일러플레이트 코드를 생성하였습니다.

---
+++ src/main/java/com/kwu/lastdance/iambackend/IamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kwu.lastdance.iambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamBackendApplication.class, args);
+	}
+
+}
