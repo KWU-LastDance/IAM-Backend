9bdf5b23903f3d4fc3150a7dbd77d830614ab15b
Hyunjun KIM <hihj070914@icloud.com>
2024-06-07 16:57:43 +0900

    feat: products controller - products와 관련된 엔드포인트들을 정의하는 컨트롤러를 작성하였습니다.

---
+++ app/api/v1/endpoints/products_controller.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.db.session import get_db
+from app.repositories.products_repository import ProductsRepository
+from app.schemas.products import Product, ProductCreate
+from app.services.products_service import ProductsService
+
+router = APIRouter()
+
+products_repository = ProductsRepository()
+products_service = ProductsService(products_repository)
+
+
+@router.get("/", response_model=list[Product])
+async def get_all_products(db: Session = Depends(get_db)):
+    return products_service.get_all(db)
+
+
+@router.post("/", response_model=Product)
+async def create_product(product_create: ProductCreate, db: Session = Depends(get_db)):
+    return products_service.create(product_create, db)
+
+
+@router.get("/{product_id}", response_model=Product)
+async def get_product_by_id(product_id: int, db: Session = Depends(get_db)):
+    return products_service.get_by_id(product_id, db)
+
+
+@router.put("/{product_id}", response_model=Product)
+async def update_product(product_id: int, product_create: ProductCreate, db: Session = Depends(get_db)):
+    return products_service.update(product_id, product_create, db)
+
+
+@router.delete("/{product_id}")
+async def delete_product(product_id: int, db: Session = Depends(get_db)):
+    return products_service.delete(product_id, db)
