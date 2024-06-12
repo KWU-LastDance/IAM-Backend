e23b2541501ad278123bd00c83db24ebab683c9c
Hyunjun KIM <hihj070914@icloud.com>
2024-06-12 14:38:48 +0900

    fix: init db - docker-compose 를 통해 서버를 켤 때 테이블이 생성되지 않는 오류를 수정하였습니다.

--- app/db/session.py
+++ app/db/session.py
@@ -3,13 +3,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-# load_dotenv()
-# DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-DATABASE_URL = "sqlite:///./test.db"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+# DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(DATABASE_URL,
                        connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def init_db():
+    from app.db.base import Base
+    Base.metadata.create_all(bind=engine)
 
 
 def get_db():
--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
 from app.api.v1.endpoints import products_controller
+from app.db.session import init_db
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    init_db()
+
 
 app.include_router(products_controller.router, prefix="/products", tags=["products"])
