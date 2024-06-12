d2368894a039d8bfd03e2d0aa91c7fea7c232608
Hyunjun KIM <hihj070914@icloud.com>
2024-06-12 15:28:50 +0900

    fix: CORS middleware
    - CORS 오류를 해결하기 위해 미들웨어를 추기하였습니다.

--- app/main.py
+++ app/main.py
@@ -5,6 +5,18 @@ from app.db.session import init_db
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
