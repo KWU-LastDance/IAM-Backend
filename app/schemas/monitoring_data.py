6fd0892d233d14ce6112ed07e9c41f4dacca5dbc
ChoiJaeHeon <jaeheon0520@naver.com>
2024-10-31 05:44:09 +0900

    fix: timestamp issue

--- app/schemas/monitoring_data.py
+++ app/schemas/monitoring_data.py
@@ -1,45 +1,57 @@
 import datetime
-
-from pydantic import BaseModel
-
+from pydantic import BaseModel, validator
 
 class MonitoringDataBase(BaseModel):
     temperature: float
     humidity: float
     timestamp: datetime.datetime
 
+    @validator("timestamp", pre=True, always=True)
+    def remove_timezone(cls, v):
+        return v.replace(tzinfo=None) if v.tzinfo else v
 
 class MonitoringDataCreate(MonitoringDataBase):
     pass
 
-
 class MonitoringData(MonitoringDataBase):
     id: int
-    timestamp: datetime.datetime
+
+    @validator("timestamp", pre=True, always=True)
+    def remove_timezone(cls, v):
+        return v.replace(tzinfo=None) if v.tzinfo else v
 
     class Config:
         from_attributes = True
-
 
 class TemperatureDataResponse(BaseModel):
     id: int
     temperature: float
     timestamp: datetime.datetime
 
+    @validator("timestamp", pre=True, always=True)
+    def remove_timezone(cls, v):
+        return v.replace(tzinfo=None) if v.tzinfo else v
+
     class Config:
         from_attributes = True
-
 
 class HumidityDataResponse(BaseModel):
     id: int
     humidity: float
     timestamp: datetime.datetime
 
+    @validator("timestamp", pre=True, always=True)
+    def remove_timezone(cls, v):
+        return v.replace(tzinfo=None) if v.tzinfo else v
+
     class Config:
         from_attributes = True
-
 
 class CurrentDataResponse(BaseModel):
     temperature: float
     humidity: float
     timestamp: datetime.datetime
+
+    @validator("timestamp", pre=True, always=True)
+    def remove_timezone(cls, v):
+        return v.replace(tzinfo=None) if v.tzinfo else v
